19f7645914eeecc38090b583d4a96326023d4f69
jkrishna12 <152908825+jkrishna12@users.noreply.github.com>
2024-03-14 18:50:39 +0000

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ from plots import dashboard_plot as dplt
 from plots import position_plot as pplt
 from plots import dividend_plot as div_plt
 
-@st.cache_data
+@st.cache_data(ttl = '15minutes')
 def load_data(t212_api_key):
     """
     Function takes in api key and returns portfolio, balance and 
