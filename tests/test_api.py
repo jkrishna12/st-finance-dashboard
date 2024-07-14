ef08bbec52e3bd81d09f1f1ffb14a421a518f6e4
jkrishna12 <krishnachintamreddy9@gmail.com>
2024-07-14 18:33:32 +0100

    returned back to pytest

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,54 +1,7 @@
 import os
 from dotenv import load_dotenv, find_dotenv
-# from src.api_connection import *
+from src.api_connection import *
 import pytest
-
-# load_dotenv(find_dotenv())
-
-# try:
-
-#     T212_API_KEY = os.getenv('T212_API_KEY')
-
-#     print(T212_API_KEY)
-
-# except:
-
-#     T212_API_KEY = 'hello'
-#     print("except block")
-#     print(T212_API_KEY)
-
-# @pytest.mark.skip(reason="not testing this")
-# def test_get_portfolio():
-
-#     portfolio_reponse = get_portfolio(T212_API_KEY)
-
-#     assert portfolio_reponse.status_code == 200
-    
-#     assert portfolio_reponse.headers["Content-Type"] == 'application/json'
-
-#     portfolio_data = portfolio_reponse.json()
-
-#     assert len(portfolio_data[0]) == 11
-
-#     return
-
-def test_github_secrets():
-
-    load_dotenv(find_dotenv())
-
-    try:
-        print('try block')
-
-        TEST_KEY = os.getenv('TEST')
-
-        print(TEST_KEY)
-
-    except:
-        print('except block')
-
-        TEST_KEY = os.environ["test_var"]
-
-        print(TEST_KEY)
 
 def get_secret(key):
     """
@@ -61,7 +14,6 @@ def get_secret(key):
 
     if env == 'local':  # Check for local environment variable
     # Load from .env file (assuming it's present locally)
-
 
         local_key = os.getenv(key)
 
@@ -76,9 +28,23 @@ def get_secret(key):
 
         print(f"deployment key {deployment_key}")
 
-        for i in deployment_key:
-            print(i)
-
         return deployment_key
 
-T212_API_KEY = get_secret('TEST')
+T212_API_KEY = get_secret('T212_API_KEY')
+
+
+def test_get_portfolio():
+
+    portfolio_reponse = get_portfolio(T212_API_KEY)
+
+    assert portfolio_reponse.status_code == 200
+    
+    assert portfolio_reponse.headers["Content-Type"] == 'application/json'
+
+    portfolio_data = portfolio_reponse.json()
+
+    assert len(portfolio_data[0]) == 11
+
+    return
+
+
